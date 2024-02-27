58a1e673901d38d4c63467f08efaaf8dd138eb29
MINGJI <106254888+MiN6JI@users.noreply.github.com>
2024-02-28 00:51:14 +0530

    Added app folder

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth; //Calls the authentication
+use Hash; //Use to set password
+
+class RegisterController extends Controller
+{
+    public function store(Request $request){
+        $input = $request->all();
+
+        User::create([
+            'name' => $input['name'],
+            'email' => $input['email'],
+            'password' => Hash::make($input['password']),
+        ]);
+            return response() ->json(['status' => true,
+                                      'message' => "Registration Sucess"
+        ]);
+    }
+}
